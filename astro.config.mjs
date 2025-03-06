337d7a37d5823f1f666c0522fd635f0681d743c7
Yogendra <147186658+yogendra5556@users.noreply.github.com>
2025-03-06 23:42:33 +0530

    Initialize tb/update-to-tailwind-v4

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from 'astro/config';
+import netlify from '@astrojs/netlify';
+import react from '@astrojs/react';
+import tailwindcss from '@tailwindcss/vite';
+
+// https://astro.build/config
+export default defineConfig({
+    vite: {
+        plugins: [tailwindcss()]
+    },
+    integrations: [react()],
+    adapter: netlify()
+});
